5b1f22936431c3bc662784af7cb101b516f67e99
theboyGolden <preciousstick15@gmail.com>
2024-01-13 23:16:43 +0000

    Work on the hero section currently in progress

---
+++ GOLDEN-MART/src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import MobNavbar from "./MobNavbar";
+import Navbar from "./Navbar";
+
+const Hero = () => {
+  return (
+    <div className="container">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-2 gap 8">
+        <div className="xl:col-span-2 xl:row-start-1 xl:row-end-[-1] relative">
+          
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
