4447fac077767c17db6f1f685623db817ac0a144
theboyGolden <preciousstick15@gmail.com>
2024-01-13 23:22:58 +0000

    Images added

--- GOLDEN-MART/src/components/Hero.tsx
+++ GOLDEN-MART/src/components/Hero.tsx
@@ -1,12 +1,10 @@
-import MobNavbar from "./MobNavbar";
-import Navbar from "./Navbar";
-
+import GOLDEN_MART_2 from '../images/GOLDEN_MART_2.jpg';
 const Hero = () => {
   return (
     <div className="container">
       <div className="grid xl:grid-cols-3 xl:grid-rows-2 gap 8">
         <div className="xl:col-span-2 xl:row-start-1 xl:row-end-[-1] relative">
-          
+
 
         </div>
       </div>
