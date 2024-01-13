17e8ab9baf0a209ef7e44a8671a7a135cf7fa44b
theboyGolden <preciousstick15@gmail.com>
2024-01-13 23:34:53 +0000

    Golden Mart banner succesfully added to Hero section

--- GOLDEN-MART/src/App.tsx
+++ GOLDEN-MART/src/App.tsx
@@ -1,11 +1,13 @@
 import MobNavbar from "./components/MobNavbar";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <MobNavbar />
+      <Hero />
     </main>
   );
 };
--- GOLDEN-MART/src/components/Hero.tsx
+++ GOLDEN-MART/src/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
     <div className="container">
       <div className="grid xl:grid-cols-3 xl:grid-rows-2 gap 8">
         <div className="xl:col-span-2 xl:row-start-1 xl:row-end-[-1] relative">
-
+        <img className="w-full h-full object-cover rounded-lg" src={GOLDEN_MART_2} alt="Golden Mart Flyer" />
 
         </div>
       </div>
