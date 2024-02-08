2f808a6fe7c0ae093d8deff824416362e2dd38e2
theboyGolden <preciousstick15@gmail.com>
2024-02-08 16:03:28 +0000

    added some prospective logo pics

--- GOLDEN-MART/src/App.tsx
+++ GOLDEN-MART/src/App.tsx
@@ -1,22 +1,14 @@
 import MobNavbar from "./components/MobNavbar";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import LoginPage from "./pages/LoginPage";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-      <main>
+    <main>
       <Navbar />
       <MobNavbar />
       <Hero />
-      </main>
-      <Route path="/LoginPage" element={<LoginPage/>} />
-      </Routes>
-    </Router>
-    
+    </main>
   );
 };
 
