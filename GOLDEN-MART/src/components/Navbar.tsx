5c3c67d264d573ab52f25a6e654e5a5124c1e324
theboyGolden <preciousstick15@gmail.com>
2024-02-08 17:22:48 +0000

    .

--- GOLDEN-MART/src/components/Navbar.tsx
+++ GOLDEN-MART/src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     return (
     <div className="sticky top-0 bg-white z-10">
         <div className="container hidden lg:block">
-            <div className = "flex justify-between items-center p-8">
+            <div className = "flex justify-between items-center p--8">
             <img className="rounded h-36 w-auto" src={Logo} alt="Golden Mart Logo" />
 
 
