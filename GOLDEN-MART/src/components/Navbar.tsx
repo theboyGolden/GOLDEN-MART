5b70a37a5c513b71d1ec7914c919a77e7f84dbda
theboyGolden <preciousstick15@gmail.com>
2024-02-08 16:15:14 +0000

    Adjusted the dimensions of the logo to fit into the navbar

--- GOLDEN-MART/src/components/Navbar.tsx
+++ GOLDEN-MART/src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className="sticky top-0 bg-white z-10">
         <div className="container hidden lg:block">
             <div className = "flex justify-between items-center p-8">
-            <img className="rounded h-16 w-auto" src={Logo} alt="Golden Mart Logo" />
+            <img className="rounded h-36 w-auto" src={Logo} alt="Golden Mart Logo" />
 
 
                 <div className="relative w-full max-w-[500px]">
